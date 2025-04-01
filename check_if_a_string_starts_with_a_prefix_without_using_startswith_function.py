73deccc48dcf9e3f0373c043e1ac98a756005d05
Angela <angeladejuan55@gmail.com>
2025-04-01 21:59:18 +0800

    Asked for user input and stored it in a variable

--- check_if_a_string_starts_with_a_prefix_without_using_startswith_function.py
+++ check_if_a_string_starts_with_a_prefix_without_using_startswith_function.py
@@ -1,4 +1,4 @@
-#Ask the user for a string
+full_text = input("Enter a string/word:") #Ask the user for a string
 #Ask the user for a prefix to check
 #Compare the first character of the string with the prefix
 #Display whether the string starts with the given prefix 
