0f46fcbc9881b266904dc88d56f379cdb8773317
Angela <angeladejuan55@gmail.com>
2025-04-01 21:12:08 +0800

    Pseudocode created

---
+++ check_if_all_characters_are_lowercase_without_islower_function.py
@@ -0,0 +1,5 @@
+#Ask user for a string
+#Assume that the string is all lowercase
+#Loop through each character
+    #If there is any detected character between 'A' and 'Z', change the assumption to False and break
+#Print results
