c14dd76e31c5debe3c6697040e73066943d0ef34
Angela <angeladejuan55@gmail.com>
2025-04-01 22:14:15 +0800

    Asked for desired width

--- right_justify_text_without_using_rjust_function.py
+++ right_justify_text_without_using_rjust_function.py
@@ -1,5 +1,5 @@
 text_to_align = input("Enter a string/word: ") #Ask the user for a string
-#Ask the user for the total width
+desired_width = int(input("Enter total width: ")) #Ask the user for the total width
 #Calculate the spaces needed: total width - length of string
 #Add spaces before the string
 #Display the result
