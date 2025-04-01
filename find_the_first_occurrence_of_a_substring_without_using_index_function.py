eefbdf70b784ad49857301940c1a0c5470205dbd
Angela <angeladejuan55@gmail.com>
2025-04-01 23:10:06 +0800

    Assumed that substring has not been found yet

--- find_the_first_occurrence_of_a_substring_without_using_index_function.py
+++ find_the_first_occurrence_of_a_substring_without_using_index_function.py
@@ -1,5 +1,7 @@
 search_text = input("Enter a string: ") #Ask the user for a string
 substring_to_find = input("Enter substring: ")#Ask for a substring to find
+
+first_occurence_index = -1 
 #Checking for the position of the substring by looping through the string
 #If there's a match, stop or break
 #Display the results
