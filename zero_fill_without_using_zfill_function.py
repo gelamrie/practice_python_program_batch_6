42b7c5893cbcd76d700796b8c3b8b51620369c96
Angela <angeladejuan55@gmail.com>
2025-04-01 22:27:09 +0800

    Added the zeros to the string

--- zero_fill_without_using_zfill_function.py
+++ zero_fill_without_using_zfill_function.py
@@ -2,5 +2,5 @@ string = input("Enter a string/word: ") #Ask the user for a string
 required_width = int(input("Enter total width: "))#Ask for the required total width
 
 zeros_needed = max(0, required_width - len(string)) #Compute the number of zeros needed: required width - length of string
-#Add zeros before the string
+zero_filled_string = "0" * zeros_needed + string #Add zeros before the string
 #Print results
