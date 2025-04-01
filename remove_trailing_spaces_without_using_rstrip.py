be60b2109459f154354591092809d589d467b57a
Angela <angeladejuan55@gmail.com>
2025-04-01 20:02:10 +0800

    Initialized a variable to track the last non_space character

--- remove_trailing_spaces_without_using_rstrip.py
+++ remove_trailing_spaces_without_using_rstrip.py
@@ -1,7 +1,6 @@
-#Ask the user to enter a string with trailing spaces and store it in a variable
-user_text = input("Enter a string with trailing spaces: ")
+user_text = input("Enter a string with trailing spaces: ") #Ask the user to enter a string with trailing spaces and store it in a variable
+trim_position = len(user_text) - 1 #Create a variable to track the last non-space character in the string
 
-#Create a variable to track the last non-space character in the string
 #To check for spaces, loop from the end of the string
 #Slice the string up to the last non-space character
 #Print results 
