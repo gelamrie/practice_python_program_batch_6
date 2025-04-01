84bfc6834240df4cd7d079f11996ffbc29702bce
Angela <angeladejuan55@gmail.com>
2025-04-01 20:51:08 +0800

    Asked the user for a string or word and stored it in a variable

--- convert_to_uppercase_manually.py
+++ convert_to_uppercase_manually.py
@@ -1,4 +1,4 @@
-#Ask the user for a string
+lowercase_text = input("Enter a string/word: ") #Ask the user for a string
 #Add a variable for uppercase text
 #Loop through each character in the string
     #If the character if between "a" and "z", convert it to uppercase
