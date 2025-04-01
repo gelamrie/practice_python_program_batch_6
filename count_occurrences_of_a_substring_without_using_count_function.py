fd1136c6826bfb4c9aa5bc3042fd9ebd5ba159d4
Angela <angeladejuan55@gmail.com>
2025-04-01 22:44:41 +0800

    Asked for a substring

--- count_occurrences_of_a_substring_without_using_count_function.py
+++ count_occurrences_of_a_substring_without_using_count_function.py
@@ -1,5 +1,5 @@
 main_text = input("Enter a string: ") #Ask the user for a string
-#Ask for a substring to count
+substring_to_count = input("Enter a substring: ") #Ask for a substring to count
 #Initialize a counter to 0
 #Check if the substring matches at each index by looping through the string
 #Display the number of occurrences
