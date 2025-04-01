ba9b6aee1410db862f561a65216a3a83a64be48c
Angela <angeladejuan55@gmail.com>
2025-04-01 23:02:34 +0800

    Pseudocode created

---
+++ find_the_first_occurrence_of_a_substring_without_using_index_function.py
@@ -0,0 +1,5 @@
+#Ask the user for a string
+#Ask for a substring to find
+#Checking for the position of the substring by looping through the string
+#If there's a match, stop or break
+#Display the results
