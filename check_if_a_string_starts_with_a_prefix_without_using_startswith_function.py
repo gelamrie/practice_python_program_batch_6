b4b1b770a8553730a18e2995d1fe5375f7af48e7
Angela <angeladejuan55@gmail.com>
2025-04-01 22:04:42 +0800

    Comparing the first characters of the string with the prefix entered

--- check_if_a_string_starts_with_a_prefix_without_using_startswith_function.py
+++ check_if_a_string_starts_with_a_prefix_without_using_startswith_function.py
@@ -1,4 +1,5 @@
 full_text = input("Enter a string/word:") #Ask the user for a string
 prefix_to_check = input("Enter prefix: ") #Ask the user for a prefix to check
-#Compare the first character of the string with the prefix
+
+starts_with_prefix = full_text[:len(prefix_to_check)] == prefix_to_check #Compare the first character of the string with the prefix
 #Display whether the string starts with the given prefix 
