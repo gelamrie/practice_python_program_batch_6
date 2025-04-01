9dd9c0c19dc0b663c356768a0cfa1ca31b66da92
Angela <angeladejuan55@gmail.com>
2025-04-01 20:49:40 +0800

    Pseudocode created

---
+++ convert_to_uppercase_manually.py
@@ -0,0 +1,6 @@
+#Ask the user for a string
+#Add a variable for uppercase text
+#Loop through each character in the string
+    #If the character if between "a" and "z", convert it to uppercase
+    #If not, add the the character as is
+#Print results
