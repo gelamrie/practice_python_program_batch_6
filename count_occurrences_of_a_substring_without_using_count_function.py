0d0804e5df4c5c184e6e472cf111eb562657e381
Angela <angeladejuan55@gmail.com>
2025-04-01 22:39:47 +0800

    Pseudocode added

---
+++ count_occurrences_of_a_substring_without_using_count_function.py
@@ -0,0 +1,5 @@
+#Ask the user for a string
+#Ask for a substring to count
+#Initialize a counter to 0
+#Check if the substring matches at each index by looping through the string
+#Display the number of occurrences
