20d7befbca2d425986899d19f65caa78b3f253cc
Angela <angeladejuan55@gmail.com>
2025-04-01 20:30:04 +0800

    Pseudocode created to serve as a guide

---
+++ remove_suffix_without_using_removesuffix_function.py
@@ -0,0 +1,6 @@
+#Ask the user for a string
+#Ask the user for a suffix to remove
+#Check if the string end with the suffix
+    #If yes, remove the suffix by slicing
+    #If no, keep the strings unchanged
+#Display the results 
