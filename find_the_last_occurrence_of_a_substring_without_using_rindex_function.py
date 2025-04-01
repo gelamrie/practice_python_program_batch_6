b3ec63adb56936a0a9a408fe322e1a021ef91152
Angela <angeladejuan55@gmail.com>
2025-04-01 23:20:10 +0800

    Pseudocode creaed and asked the user for input

---
+++ find_the_last_occurrence_of_a_substring_without_using_rindex_function.py
@@ -0,0 +1,5 @@
+search_text = input("Enter a string: ") #Ask the user for a string
+#Ask for a substring to find from the end
+#Check if the substring matches at each index by looping through the string backwards
+#Stop if there's a match
+#Display the index
