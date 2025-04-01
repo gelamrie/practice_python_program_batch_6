cd575c078c727ab4784fec785a67d9a1e70a8c21
Angela <angeladejuan55@gmail.com>
2025-04-01 20:31:38 +0800

    Asked the user for a string or word and stored it in a variable

--- remove_suffix_without_using_removesuffix_function.py
+++ remove_suffix_without_using_removesuffix_function.py
@@ -1,4 +1,6 @@
 #Ask the user for a string
+original_text = input("Enter a string/word: ")
+
 #Ask the user for a suffix to remove
 #Check if the string end with the suffix
     #If yes, remove the suffix by slicing
