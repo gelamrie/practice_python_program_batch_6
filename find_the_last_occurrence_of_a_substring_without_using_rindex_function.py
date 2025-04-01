bd680ff86c20764bae3add94223d305620e64098
Angela <angeladejuan55@gmail.com>
2025-04-01 23:23:43 +0800

    Looping backwards

--- find_the_last_occurrence_of_a_substring_without_using_rindex_function.py
+++ find_the_last_occurrence_of_a_substring_without_using_rindex_function.py
@@ -2,6 +2,7 @@ search_text = input("Enter a string: ") #Ask the user for a string
 substring_to_find = input("Enter substring to find from the end: ") #Ask for a substring to find from the end
 
 last_occurrence_index = -1
-#Check if the substring matches at each index by looping through the string backwards
+for index in range(len(search_text) - len(substring_to_find), -1, -1): #Check if the substring matches at each index by looping through the string backwards
+    if search_text[index:index+len(substring_to_find)] == substring_to_find: 
 #Stop if there's a match
 #Display the index
