a5a05109bdb69162c5a8e38b239aa150542a96ac
Angela <angeladejuan55@gmail.com>
2025-04-01 23:25:14 +0800

    Break if match is found

--- find_the_last_occurrence_of_a_substring_without_using_rindex_function.py
+++ find_the_last_occurrence_of_a_substring_without_using_rindex_function.py
@@ -4,5 +4,6 @@ substring_to_find = input("Enter substring to find from the end: ") #Ask for a s
 last_occurrence_index = -1
 for index in range(len(search_text) - len(substring_to_find), -1, -1): #Check if the substring matches at each index by looping through the string backwards
     if search_text[index:index+len(substring_to_find)] == substring_to_find: 
-#Stop if there's a match
+        last_occurrence_index = index
+        break #Stop if there's a match
 #Display the index
