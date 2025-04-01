21b0937fdaec22a7ae3be0122df0c062f448a9c8
Angela <angeladejuan55@gmail.com>
2025-04-01 22:49:45 +0800

    If the substring was found in the string, add 1 to the occurrence count

--- count_occurrences_of_a_substring_without_using_count_function.py
+++ count_occurrences_of_a_substring_without_using_count_function.py
@@ -3,4 +3,6 @@ substring_to_count = input("Enter a substring: ") #Ask for a substring to count
 
 occurrence_count = 0 #Initialize a counter to 0
 for index in range(len(main_text) - len(substring_to_count) + 1): #Check if the substring matches at each index by looping through the string
+    if main_text[index:index+len(substring_to_count)] == substring_to_count:
+        occurrence_count += 1
 #Display the number of occurrences
