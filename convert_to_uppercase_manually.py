1c377e2d159afb1b17c9500b006031eb67f972c1
Angela <angeladejuan55@gmail.com>
2025-04-01 20:58:00 +0800

    Indention error fixed and completed the if-else condition

--- convert_to_uppercase_manually.py
+++ convert_to_uppercase_manually.py
@@ -3,6 +3,7 @@ uppercase_text = "" #Add a variable for uppercase text
 
 for character in lowercase_text: #Loop through each character in the string
     if 'a' <= character <= 'z': #If the character if between "a" and "z", convert it to uppercase
-    uppercase_text += chr(ord(character) - 32) 
-    #If not, add the the character as is
+        uppercase_text += chr(ord(character) - 32) 
+    else: 
+        uppercase_text += character #If not, add the the character as is
 #Print results
