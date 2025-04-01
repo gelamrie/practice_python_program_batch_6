c5cc1681246cf773b59b37fdbf41e42085e5e682
Angela <angeladejuan55@gmail.com>
2025-04-01 20:53:17 +0800

    Loop created to check the characters of the string or word input by the user

--- convert_to_uppercase_manually.py
+++ convert_to_uppercase_manually.py
@@ -1,6 +1,7 @@
 lowercase_text = input("Enter a string/word: ") #Ask the user for a string
 uppercase_text = "" #Add a variable for uppercase text
-#Loop through each character in the string
+
+for character in lowercase_text: #Loop through each character in the string
     #If the character if between "a" and "z", convert it to uppercase
     #If not, add the the character as is
 #Print results
