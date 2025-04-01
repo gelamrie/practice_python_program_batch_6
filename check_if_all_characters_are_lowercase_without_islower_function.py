3ee9500fd40ce4f5c3dafcc71b3e39adff1d665c
Angela <angeladejuan55@gmail.com>
2025-04-01 21:32:56 +0800

    Loop created to check the characters of the input string

--- check_if_all_characters_are_lowercase_without_islower_function.py
+++ check_if_all_characters_are_lowercase_without_islower_function.py
@@ -1,5 +1,7 @@
 input_text = input("Enter a string/word: ")#Ask user for a string
 all_lowercase = True #Assume that the string is all lowercase
+
 #Loop through each character
+for character in input_text:
     #If there is any detected character between 'A' and 'Z', change the assumption to False and break
 #Print results
