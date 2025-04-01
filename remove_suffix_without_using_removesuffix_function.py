b0efeef0af0c9960e1d3df073dc34f9961eb5582
Angela <angeladejuan55@gmail.com>
2025-04-01 20:37:05 +0800

    Checking if the string ends with the suffix

--- remove_suffix_without_using_removesuffix_function.py
+++ remove_suffix_without_using_removesuffix_function.py
@@ -2,6 +2,7 @@ original_text = input("Enter a string/word: ") #Ask the user for a string
 suffix_to_remove = input("Enter suffix to remove: ") #Ask the user for a suffix to remove
 
 #Check if the string end with the suffix
+if original_text.endswith(suffix_to_remove):
     #If yes, remove the suffix by slicing
     #If no, keep the strings unchanged
 #Display the results 
