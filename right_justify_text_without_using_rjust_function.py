e49c612d9319cc0df1f3969847c904b771ac578c
Angela <angeladejuan55@gmail.com>
2025-04-01 22:11:45 +0800

    Asked for the user input and stored it in text_to_align variable

--- right_justify_text_without_using_rjust_function.py
+++ right_justify_text_without_using_rjust_function.py
@@ -1,4 +1,4 @@
-#Ask the user for a string
+text_to_align = input("Enter a string/word: ") #Ask the user for a string
 #Ask the user for the total width
 #Calculate the spaces needed: total width - length of string
 #Add spaces before the string
