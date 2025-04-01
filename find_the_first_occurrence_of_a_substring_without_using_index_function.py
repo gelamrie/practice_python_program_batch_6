e7df49aa3e1d139d87f70d999a0a1f7a3d89621e
Angela <angeladejuan55@gmail.com>
2025-04-01 23:11:01 +0800

    Loop created

--- find_the_first_occurrence_of_a_substring_without_using_index_function.py
+++ find_the_first_occurrence_of_a_substring_without_using_index_function.py
@@ -2,6 +2,6 @@ search_text = input("Enter a string: ") #Ask the user for a string
 substring_to_find = input("Enter substring: ")#Ask for a substring to find
 
 first_occurence_index = -1 
-#Checking for the position of the substring by looping through the string
+for index in range(len(search_text) - len(substring_to_find) + 1): #Checking for the position of the substring by looping through the string
 #If there's a match, stop or break
 #Display the results
