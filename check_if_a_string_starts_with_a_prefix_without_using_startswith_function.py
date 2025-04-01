220c8744603340b0d08736ba12b7e995284fb126
Angela <angeladejuan55@gmail.com>
2025-04-01 21:57:21 +0800

    Pseudocode created

---
+++ check_if_a_string_starts_with_a_prefix_without_using_startswith_function.py
@@ -0,0 +1,4 @@
+#Ask the user for a string
+#Ask the user for a prefix to check
+#Compare the first character of the string with the prefix
+#Display whether the string starts with the given prefix 
