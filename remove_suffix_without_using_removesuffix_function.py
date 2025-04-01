9bd126309b9b0be26ff6507a2d86875b14566fc4
Angela <angeladejuan55@gmail.com>
2025-04-01 20:44:12 +0800

    Displaying results

--- remove_suffix_without_using_removesuffix_function.py
+++ remove_suffix_without_using_removesuffix_function.py
@@ -6,5 +6,5 @@ if original_text.endswith(suffix_to_remove):
     modified_text = original_text[:-len(suffix_to_remove)] #If yes, remove the suffix by slicing
 else: #If no, keep the strings unchanged
     modified_text = original_text
-    
-#Display the results 
+
+print("String after removing suffix:", modified_text) #Display the results
