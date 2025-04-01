7a7a477dabb37805e6976f05933eda063b8909ae
Angela <angeladejuan55@gmail.com>
2025-04-01 22:05:20 +0800

    Print results

--- check_if_a_string_starts_with_a_prefix_without_using_startswith_function.py
+++ check_if_a_string_starts_with_a_prefix_without_using_startswith_function.py
@@ -2,4 +2,4 @@ full_text = input("Enter a string/word:") #Ask the user for a string
 prefix_to_check = input("Enter prefix: ") #Ask the user for a prefix to check
 
 starts_with_prefix = full_text[:len(prefix_to_check)] == prefix_to_check #Compare the first character of the string with the prefix
-#Display whether the string starts with the given prefix 
+print("Does the string start with the prefix?", starts_with_prefix) #Display whether the string starts with the given prefix 
