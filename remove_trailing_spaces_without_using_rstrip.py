8d0e3532a425604f1c379ab49c710e5139cf88de
Angela <angeladejuan55@gmail.com>
2025-04-01 19:57:24 +0800

    I've created the pseudocode

---
+++ remove_trailing_spaces_without_using_rstrip.py
@@ -0,0 +1,5 @@
+#Ask the user to enter a string with trailing spaces and store it in a variable
+#Create a variable to track the last non-space character in the string
+#To check for spaces, loop from the end of the string
+#Slice the string up to the last non-space character
+#Print results 
